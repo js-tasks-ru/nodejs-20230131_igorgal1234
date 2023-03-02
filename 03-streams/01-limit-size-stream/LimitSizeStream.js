5d1834b68caddf4b1784d1f21996ae079771695d
ihorhalchenko <ihorhalchenkoit@gmail.com>
2023-03-02 16:30:28 +0200

    Resolving task Стрим с лимитом передачи данных

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,22 @@
-const stream = require('stream');
-const LimitExceededError = require('./LimitExceededError');
+const stream = require("stream");
+const LimitExceededError = require("./LimitExceededError");
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.bytesTransferred = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.bytesTransferred += chunk.byteLength;
 
+    if (this.bytesTransferred > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
