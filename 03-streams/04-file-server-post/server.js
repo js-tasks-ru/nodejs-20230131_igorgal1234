abe979a9c5d111086cb2ce1e034030accdd14d46
ihorhalchenko <ihorhalchenkoit@gmail.com>
2023-03-11 23:07:02 +0200

    Work version 2, task - Файловый сервер - запись файла

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -16,60 +16,64 @@ server.on("request", (req, res) => {
   switch (req.method) {
     case "POST":
       if (pathname.includes("/")) {
-        res.statusCode = 400;
-        res.end("Nested folders are not supported");
-      }
+        endResponse(400, "Nested folders are not supported");
+      } else {
+        fs.access(filepath, (err) => {
+          if (err && err.code === "ENOENT") {
+            const stream = fs.createWriteStream(filepath);
+            const limitSizeStream = new LimitSizeStream({ limit: 1e6 });
 
-      fs.access(filepath, (err) => {
-        if (err === null) {
-          res.statusCode = 409;
-          res.end("File already exists");
-        } else if (err.code === "ENOENT") {
-          const stream = fs.createWriteStream(filepath);
-          const limitSizeStream = new LimitSizeStream({ limit: 1e6 });
-
-          req.pipe(limitSizeStream).pipe(stream);
-
-          stream.on("error", (err) => {
-            res.statusCode = 500;
-            res.end(err);
-          });
-
-          limitSizeStream.on("error", (err) => {
-            res.statusCode = 500;
-            fs.unlink(filepath, (err) => {
-              if (err) {
-                throw Error("Error during file removing.");
+            limitSizeStream.on("error", (err) => {
+              if (err.code === "LIMIT_EXCEEDED") {
+                endResponse(413, "File size is too big. Max size is 1MB");
+              } else {
+                endResponse(500, "Something went wrong3");
               }
+              stream.destroy();
+              removeFile(filepath);
             });
 
-            if (err.code === "LIMIT_EXCEEDED") {
-              res.statusCode = 413;
-              res.end("File size is too big. Max size is 1MB");
-            } else {
-              res.end("Something went wrong");
-            }
-          });
+            stream.on("error", () => {
+              endResponse(500, "Something went wrong");
+            });
 
-          req.on("aborted", () => {
-            stream.destroy();
-            limitSizeStream.destroy();
-          });
-        } else {
-          res.end("Something went wrong");
-        }
-      });
+            req.on("aborted", () => {
+              stream.destroy();
+              limitSizeStream.destroy();
+              removeFile(filepath);
+            });
+
+            req.pipe(limitSizeStream).pipe(stream);
+          } else if (err) {
+            endResponse(500, "Something went wrong");
+          } else {
+            endResponse(409, "File already exists");
+          }
+        });
+      }
 
       break;
 
     default:
-      res.statusCode = 501;
-      res.end("Not implemented");
+      endResponse(501, "Not implemented");
   }
 
   req.on("end", () => {
-    res.end("Ok");
+    endResponse(201, "Ok");
   });
+
+  function endResponse(statusCode, message) {
+    res.statusCode = statusCode;
+    res.end(message);
+  }
+
+  function removeFile(filepath) {
+    fs.unlink(filepath, (err) => {
+      if (err) {
+        endResponse(500, "Something went wrong2");
+      }
+    });
+  }
 });
 
 module.exports = server;
