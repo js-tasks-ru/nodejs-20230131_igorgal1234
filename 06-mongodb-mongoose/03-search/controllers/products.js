e3fbb6350faca61c9101992700f1a0d34a1cc449
ihorhalchenko <ihorhalchenkoit@gmail.com>
2023-04-08 21:49:42 +0300

    Resolved task Поиск товаров

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,24 @@
+const Product = require("../models/Product");
+const mapProduct = require("../mappers/product");
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const query = ctx.query.query;
+
+  if (!query) {
+    ctx.status = 400;
+    ctx.body = "There is no query parameter.";
+
+    return next();
+  }
+
+  const products = await Product.find(
+    {
+      $text: {
+        $search: query,
+      },
+    },
+    { score: { $meta: "textScore" } }
+  );
+
+  ctx.body = { products: products.map(mapProduct) };
 };
